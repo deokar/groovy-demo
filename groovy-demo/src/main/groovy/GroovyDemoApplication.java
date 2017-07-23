856b00029305bf62d921641ea18c57c6e803897c
deokar <amex2107>
2017-07-23 19:19:30 -0400

    Initial commit

---
+++ groovy-demo/src/main/groovy/GroovyDemoApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyDemoApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(GroovyDemoApplication.class, args);
+        }
+    }
+
